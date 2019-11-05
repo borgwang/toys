203a291df5820c599a71bdca1f6bec11fbe167cf
wangguibo <wangguibo@ainnovation.com>
2019-11-05 18:24:23 +0800

    add gini criterion

--- tree_models/decision_tree.py
+++ tree_models/decision_tree.py
@@ -3,6 +3,7 @@ from collections import Counter
 import numpy as np
 from sklearn.datasets import load_iris
 from sklearn.model_selection import train_test_split
+from sklearn.metrics import accuracy_score
 
 
 def is_numerical(val):
@@ -22,8 +23,12 @@ def divide_on_feature(Xy, feat_idx, thr):
 
 class DTNode:
 
-    def __init__(self, feat_idx=None, threshold=None, value=None, 
-                 left=None, right=None):
+    def __init__(self, 
+                 feat_idx=None, 
+                 threshold=None, 
+                 left=None, 
+                 right=None,
+                 value=None):
         self.feat_idx = feat_idx
         self.threshold = threshold
         self.value = value
@@ -38,10 +43,11 @@ class DecisionTree:
                  min_samples_split, 
                  min_impurity_split,
                  loss):
+        self.max_depth = float("inf") if max_depth is None else max_depth
         self.min_samples_split = min_samples_split
         self.min_impurity_split = min_impurity_split
-        self.max_depth = max_depth
         self.loss = loss
+
 
         self.root = None
 
@@ -50,7 +56,7 @@ class DecisionTree:
         self.loss = None
 
     def predict(self, X):
-        return [self._predict_sample(x) for x in X]
+        return np.array([self._predict_sample(x) for x in X]).reshape(-1, 1)
 
     def impurity_func(self, *args, **kwargs):
         raise NotImplementedError
@@ -80,7 +86,7 @@ class DecisionTree:
         Xy = np.concatenate((X, y), axis=1)
         n_feats = X.shape[1]
 
-        max_impurity = 0
+        max_impurity = 0.0
         best_split = None
         for col in range(n_feats):
             # for each feature
@@ -120,31 +126,52 @@ class ClassificationTree(DecisionTree):
 
     def __init__(self, 
                  criterion="gini", 
-                 max_depth=10,
+                 max_depth=None,
                  min_samples_split=2,
                  min_impurity_split=1e-7):
         super().__init__(max_depth, min_samples_split, min_impurity_split, loss=None)
-        assert criterion in ("entropy", "gini")
+        assert criterion in ("info_gain", "gain_ratio", "gini")
         self.criterion = criterion
 
     def _impurity_func(self, y, l_y, r_y):
-        if self.criterion == "gini":
-            return self._gini(y, l_y, r_y)
+        if self.criterion == "info_gain":
+            return self.__info_gain(y, l_y, r_y)
+        elif self.criterion == "gain_ratio":
+            return self.__gain_ratio(y, l_y, r_y)
         else:
-            return self._info_gain(y, l_y, r_y)
+            # use 1-gini_index to represent impurity since gini_index measures purity
+            return 1.0 - self.__gini_index(y, l_y, r_y)
 
-    def _gini(self, y):
-        pass
+    def __info_gain(self, y, l_y, r_y):
 
-    def _info_gain(self, y):
-        def _entropy(ys):
-            counts = dict(Counter(y.reshape(-1)))
-            probs = np.array([1. * cnt / len(ys) for cnt in counts.values()])
-            return -np.sum(probs * np.log(probs))
-        before = _entropy(y)
-        after = _entropy(l_y) + _entropy(r_y)
+        def entropy(vals):
+            cnts = dict(Counter(vals.reshape(-1)))
+            probs = np.array([1. * cnt / len(vals) for cnt in cnts.values()])
+            return -(probs * np.log(probs)).sum()
+
+        before = entropy(y)
+        after = (len(l_y) * entropy(l_y) + len(r_y) * entropy(r_y)) / len(y)
         return before - after
         
+    def __gain_ratio(self, y, l_y, r_y):
+        info_gain = self.__info_gain(y, l_y, r_y)
+        # compute IV
+        l_f = len(l_y) / len(y)
+        r_f = len(r_y) / len(y)
+        iv = -(l_f * np.log(l_f) + r_f * np.log(r_f))
+        return info_gain / iv
+
+    def __gini_index(self, y, l_y, r_y):
+
+        def gini(vals):
+            cnts = Counter(vals.reshape(-1)).values()
+            probs = np.array([1. * cnt / len(vals) for cnt in cnts])
+            return 1.0 - np.sum(probs ** 2)
+
+        l_f = len(l_y) / len(y)
+        r_f = len(r_y) / len(y)
+        return l_f * gini(l_y) + r_f * gini(r_y)
+
     def _aggregation_func(self, y):
         # majority vote
         res = Counter(y.reshape(-1))
@@ -157,6 +184,7 @@ class RegressionTree(DecisionTree):
         pass
 
     def _aggregation_func(self):
+        # simple average
         pass
 
 
@@ -167,14 +195,14 @@ if __name__ == "__main__":
     y = y.reshape((-1, 1))
 
     train_x, test_x, train_y, test_y = train_test_split(X, y, test_size=0.3)
-    model = ClassificationTree(criterion="gini", max_depth=10, min_samples_split=2)
+    model = ClassificationTree(criterion="gini", max_depth=None, min_samples_split=2)
     model.fit(train_x, train_y)
     test_preds = model.predict(test_x)
 
     from sklearn.tree import DecisionTreeClassifier
-    model = DecisionTreeClassifier(criterion="gini", max_depth=10, min_samples_split=2)
+    model = DecisionTreeClassifier(criterion="gini", max_depth=None, min_samples_split=2)
     model.fit(train_x, train_y)
     test_preds2 = model.predict(test_x)
 
-    print("mse-mine: %.4f" % np.mean((test_preds - test_y)**2))
-    print("mse-sklearn: %.4f" % np.mean((test_preds2 - test_y)**2))
+    print("acc-mine: %.4f" % accuracy_score(test_y, test_preds))
+    print("acc-sklearn: %.4f" % accuracy_score(test_y, test_preds2))
